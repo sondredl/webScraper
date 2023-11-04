d2b9fe896a613ff68e9422737b89269bc54d69a0
Sondre Lind <sondredl@gmailcom>
2023-11-04 18:47:54 +0100

    change file storage to use page names

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import time
 
 url_array = []
 
-def createFileToParse(url, i):
-    filename = str(i) + ".html"
+def createFileToParse(page, url, i):
+    filename = page + ".html"
     path = "htmlFiles/" 
     path += filename
     subprocess.run(["curl", "-o", path, url])
@@ -15,14 +15,14 @@ def createContentFiles(listOfUrls):
     subprocess.run(["mkdir", "htmlFiles"])
     iterator = 1
     for i in listOfUrls:
-        createFileToParse(i[2], iterator)
+        createFileToParse(i[1], i[2], iterator)
         iterator += 1
 
 dn  = ["", "dn",    "https://www.dn.no/"]
 nrk = ["", "nrk",   "https://www.nrk.no/"]
 e24 = ["", "e24",   "https://www.e24.no/"]
 euronext = ["", "euronext",   "https://live.euronext.com/en/markets/oslo"]
-# url_array.append(dn)
+url_array.append(dn)
 url_array.append(nrk)
 # url_array.append(e24)
 url_array.append(euronext)
