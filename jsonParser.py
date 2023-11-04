056dc91e7cd1ff0a766f196b5eaf2b467088f837
Sondre Lind <sondredl@gmailcom>
2023-11-04 21:35:54 +0100

    json parsing for web pages, html tags and search words

--- jsonParser.py
+++ jsonParser.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 import json
 
-json_file_path = 'webPages.json'  
-with open(json_file_path, 'r') as json_file:
-    web_pages = json.load(json_file)
+def webPages():
+    json_file_path = 'webPages.json'  
+    with open(json_file_path, 'r') as json_file:
+        web_pages = json.load(json_file)
 
-for page in web_pages:
-    print(page)
+    for page in web_pages:
+        print(page)
+
+def htmlTags():
+    json_file_path = 'htmlTags.json'  
+    with open(json_file_path, 'r') as json_file:
+        htmlTags = json.load(json_file)
+
+    for tag in htmlTags:
+        print(tag)
+
+def searchWords():
+    json_file_path = 'searchWords.json'  
+    with open(json_file_path, 'r') as json_file:
+        word_list = json.load(json_file)
+
+    for word in word_list:
+        print(word)
+
+webPages()
+htmlTags()
+searchWords()
