6b92f2be39fed8b3cfe88ce08983a7db2782111a
Sondre Lind <sondredl@gmailcom>
2023-11-04 17:04:23 +0100

    before implement loop of files

--- parser.py
+++ parser.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
 
+import os
 from bs4 import BeautifulSoup
 import sqlite3
 
-# with open('1.html', 'r', encoding='utf-8') as file:
+conn = sqlite3.connect('your_database.db')
+cursor = conn.cursor()
+
+folder_path = "htmlFiles/"
+
+# for filename in os.listdir(folder_path):
 with open('htmlFiles/2.html', 'r', encoding='utf-8') as file:
     soup = BeautifulSoup(file, 'html.parser')
 
 selected_words = ['konflikt', 'krig', 'topp', 'seafood']
 
-# for paragraph in soup.find_all('tbody, h1, h2, h3, p, tbody, td, kur-newsfeed'):  # Adjust the tag based on your HTML structure
-for paragraph in soup.find_all('tbody, h1, h2, h3, p, tbody, td, tr, kur-newsfeed'):  # Adjust the tag based on your HTML structure
-    sentences = paragraph.text.split('.')
+for tag in soup.select('tbody, h1, h2, h3, p, tbody, td, kur-newsfeed'):
+    sentences = tag.text.split('.')
     selected_sentences = [sentence.strip() for sentence in sentences if any(word in sentence for word in selected_words)]
-# for paragraph in soup.find_all('div'):  # Adjust the tag based on your HTML structure
-#     sentences = paragraph.text.split('.')
-#     selected_sentences = [sentence.strip() for sentence in sentences if any(word in sentence for word in selected_words)]
-# for paragraph in soup.find_all('span'):  # Adjust the tag based on your HTML structure
-#     sentences = paragraph.text.split('.')
-#     selected_sentences = [sentence.strip() for sentence in sentences if any(word in sentence for word in selected_words)]
 
 
-conn = sqlite3.connect('your_database.db')
-cursor = conn.cursor()
 
 # Adjust the table structure based on your needs
 cursor.execute('''CREATE TABLE IF NOT EXISTS Sentences
