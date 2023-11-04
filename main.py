49172076206dbfbb019060b4d01518597cf62150
Sondre Lind <sondredl@gmailcom>
2023-11-04 16:50:32 +0100

    try parse multiple html tags and write to database

--- main.py
+++ main.py
@@ -18,18 +18,20 @@ def createContentFiles(listOfUrls):
         createFileToParse(i[2], iterator)
         iterator += 1
 
-nrk = ["", "nrk",   "https://www.nrk.no/"]
 dn  = ["", "dn",    "https://www.dn.no/"]
+nrk = ["", "nrk",   "https://www.nrk.no/"]
 e24 = ["", "e24",   "https://www.e24.no/"]
+euronext = ["", "euronext",   "https://live.euronext.com/en/markets/oslo"]
+# url_array.append(dn)
 url_array.append(nrk)
-url_array.append(dn)
-url_array.append(e24)
+# url_array.append(e24)
+url_array.append(euronext)
 
 
 def main():
-    while True:
+    # while True:
         createContentFiles(url_array)
-        time.sleep(300)
+        # time.sleep(300)
 
 if __name__ == "__main__":
     main()
