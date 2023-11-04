6fff3a552ce26ac585a6636fe4b3c0ce7b4adfbd
Sondre Lind <sondredl@gmailcom>
2023-11-04 12:22:30 +0100

    creates output folder

--- main.py
+++ main.py
@@ -2,19 +2,23 @@
 
 import subprocess
 
+url_array = []
+
 def createFileToParse(url, i):
     filename = str(i) + ".html"
-    subprocess.run(["curl", "-o", filename, url])
-
-url_array = []
-url_array.append("https://www.nrk.no/")
-url_array.append("https://www.dn.no/")
-url_array.append("https://www.e24.no/")
+    path = "htmlFiles/" 
+    path += filename
+    subprocess.run(["curl", "-o", path, url])
 
 def createContentFiles(listOfUrls):
+    subprocess.run(["mkdir", "htmlFiles"])
     iterator = 1
     for i in listOfUrls:
         createFileToParse(i, iterator)
         iterator += 1
+
+url_array.append("https://www.nrk.no/")
+url_array.append("https://www.dn.no/")
+url_array.append("https://www.e24.no/")
 
 createContentFiles(url_array)
