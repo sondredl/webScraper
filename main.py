5d429842f738f62a0f869af04a1523b7b65a7eef
Sondre Lind <sondredl@gmailcom>
2023-11-04 12:14:28 +0100

    creates loop to generate multiple html files

--- main.py
+++ main.py
@@ -2,4 +2,19 @@
 
 import subprocess
 
-subprocess.run(["curl", "-o", "content.html", "https://www.nrk.no/"])
+def createFileToParse(url, i):
+    filename = str(i) + ".html"
+    subprocess.run(["curl", "-o", filename, url])
+
+url_array = []
+url_array.append("https://www.nrk.no/")
+url_array.append("https://www.dn.no/")
+url_array.append("https://www.e24.no/")
+
+def createContentFiles(listOfUrls):
+    iterator = 1
+    for i in listOfUrls:
+        createFileToParse(i, iterator)
+        iterator += 1
+
+createContentFiles(url_array)
