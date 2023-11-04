582d96990d6ec797c6928cea98456b7afc7560df
Sondre Lind <sondredl@gmailcom>
2023-11-04 18:27:28 +0100

    adds for loop through files in dir

--- parser.py
+++ parser.py
@@ -9,24 +9,26 @@ cursor = conn.cursor()
 
 folder_path = "htmlFiles/"
 
-# for filename in os.listdir(folder_path):
-with open('htmlFiles/2.html', 'r', encoding='utf-8') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+for filename in os.listdir(folder_path):
+      if filename.endswith('.html'):
+        # print(filename)
+        file_path = os.path.join(folder_path, filename)
 
-selected_words = ['konflikt', 'krig', 'topp', 'seafood']
+        # Step 4: Parse HTML file
+        with open(file_path, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'html.parser')
 
-for tag in soup.select('tbody, h1, h2, h3, p, tbody, td, kur-newsfeed'):
-    sentences = tag.text.split('.')
-    selected_sentences = [sentence.strip() for sentence in sentences if any(word in sentence for word in selected_words)]
+        selected_words = ['konflikt', 'krig', 'topp', 'seafood', 'euronext', 'performers']
 
+        for tag in soup.select('tbody, div, h1, h2, h3, p, tbody, td, kur-newsfeed, a'):
+            sentences = tag.text.split('.')
+            selected_sentences = [sentence.strip() for sentence in sentences if any(word in sentence for word in selected_words)]
 
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Sentences
+                        (id INTEGER PRIMARY KEY AUTOINCREMENT, sentence TEXT)''')
 
-# Adjust the table structure based on your needs
-cursor.execute('''CREATE TABLE IF NOT EXISTS Sentences
-                  (id INTEGER PRIMARY KEY AUTOINCREMENT, sentence TEXT)''')
-
-for sentence in selected_sentences:
-    cursor.execute("INSERT INTO Sentences (sentence) VALUES (?)", (sentence,))
+        for sentence in selected_sentences:
+            cursor.execute("INSERT INTO Sentences (sentence) VALUES (?)", (sentence,))
 
 conn.commit()
 conn.close()
