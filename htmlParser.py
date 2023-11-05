69fe72c1c754ea49ba95ce97b7e994b0239142e3
Sondre Lind <sondredl@gmailcom>
2023-11-05 16:28:41 +0100

    adds href link to insertion

--- htmlParser.py
+++ htmlParser.py
@@ -35,8 +35,34 @@ def updateDatabase():
                     content = tag.text
                     if any(word in content for word in selected_words):
                         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        cursor.execute("INSERT INTO Sentences (filename, tag_name, sentence, timestamp) VALUES (?, ?, ?, ?)",
-                                    (filename, tag_name, content, timestamp))
+
+                        link_tag = tag.find_parent('a')
+                        if link_tag:
+                            href_link = link_tag.get('href')
+                        cursor.execute("INSERT INTO Sentences (filename, tag_name, sentence, href, timestamp) VALUES (?, ?, ?, ?, ?)",
+                                    (filename, tag_name, content, href_link, timestamp))
+                            
+
 
     conn.commit()
     conn.close()
+
+def createNewColumn(cursor, table_name, column_name):
+    # table_name = 'Sentences'
+    # new_column_name = 'new_column'
+    if not doesColumnExist(cursor, table_name, column_name):
+        # Step 3: If the column doesn't exist, add it
+        cursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {column_name} TEXT")
+        print(f"The column {column_name} has been added.")
+        return
+    print("FAILED TO ADD COLUMN TO TABLE")
+
+def doesColumnExist(cursor, table_name, column_name):
+    cursor.execute(f"PRAGMA table_info({table_name})")
+    
+    columns = cursor.fetchall()
+    # print(column_name, " in ", table_name, "DOES NOT EXIST")
+    
+    return any(column[1] == column_name for column in columns)
+
+updateDatabase()
