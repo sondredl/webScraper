623c78e13a79d35ea7bbb5b2e66fd02f33de53e4
Sondre Lind <sondredl@gmailcom>
2023-11-12 13:47:19 +0100

    fix function call name

---
+++ dev.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import jsonParser
+import htmlParser
+
+import subprocess
+import time
+
+def createFileToParse(name, url):
+    filename = name + ".html"
+    path = "htmlFiles/" 
+    path += filename
+    subprocess.run(["curl", "-L", "-o", path, url])
+
+def createContentFiles():
+    subprocess.run(["mkdir", "htmlFiles"])
+    pageList = jsonParser.webPages()
+    for page in pageList:
+        createFileToParse(page[0], page[1])
+
+
+def main():
+    # while True:
+        createContentFiles()
+        htmlParser.updateDatabase()
+        htmlParser.getWordAndUrl()
+        # subprocess.run(["rm", "-rf", "htmlFiles/"])
+        # time.sleep(300)
+
+
+if __name__ == "__main__":
+    main()
+
+
