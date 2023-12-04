96a9703ebb1126f223097909974f73e3ec969d94
Sondre Lind <sondredl@gmail.com>
2023-12-04 20:34:46 +0100

    extract article better

--- extractArticle.py
+++ extractArticle.py
@@ -7,13 +7,20 @@ with open('article.html', 'r', encoding='utf-8') as file:
 # Parse HTML using BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Extract title and text
-title = soup.title.text.strip() if soup.title else 'No Title Found'
+# Extract title and subtitle
+title_tag = soup.title
+subtitle_tag = soup.find(['h2', 'h3', 'h4', 'h5', 'h6', 'p'])
+
+title = title_tag.text.strip() if title_tag else 'No Title Found'
+subtitle = subtitle_tag.text.strip() if subtitle_tag else 'No Subtitle Found'
+
+# Extract text
 text = '\n'.join([p.text.strip() for p in soup.find_all('p')])
 
 # Write result to content.txt
 with open('content.txt', 'w', encoding='utf-8') as output_file:
-    output_file.write(f'Title: {title}\n\n')
+    output_file.write(f'Title: {title}\n')
+    output_file.write(f'Subtitle: {subtitle}\n\n')
     output_file.write(f'Text:\n{text}')
 
 print('Extraction completed. Check content.txt for the result.')
