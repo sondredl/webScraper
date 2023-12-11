5d3970f6beda5091c5fdc6e88aee43c4cb5a1591
Sondre Lind <sondredl@gmail.com>
2023-12-11 23:00:35 +0100

    changes script to function

--- src/extractArticle.py
+++ src/extractArticle.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
 
-with open("article.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
 
-soup = BeautifulSoup(html_content, "html.parser")
+def get_article_from_url(filename):
+    # with open("article.html", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
+        html_content = file.read()
 
-title_tag = soup.title
-subtitle_tag = soup.find(["h2", "h3", "h4", "h5", "h6", "p"])
+    soup = BeautifulSoup(html_content, "html.parser")
 
-title = title_tag.text.strip() if title_tag else "No Title Found"
-subtitle = subtitle_tag.text.strip() if subtitle_tag else "No Subtitle Found"
+    title_tag = soup.title
+    subtitle_tag = soup.find(["h2", "h3", "h4", "h5", "h6", "p"])
 
-text = "\n".join([p.text.strip() for p in soup.find_all("p")])
+    title = title_tag.text.strip() if title_tag else "No Title Found"
+    subtitle = subtitle_tag.text.strip() if subtitle_tag else "No Subtitle Found"
 
-with open("content.txt", "w", encoding="utf-8") as output_file:
-    output_file.write(f"Title: {title}\n")
-    output_file.write(f"Subtitle: {subtitle}\n\n")
-    output_file.write(f"Text:\n{text}")
+    text = "\n".join([p.text.strip() for p in soup.find_all("p")])
 
-print("Extraction completed. Check content.txt for the result.")
+    with open("content.txt", "w", encoding="utf-8") as output_file:
+        output_file.write(f"Title: {title}\n")
+        output_file.write(f"Subtitle: {subtitle}\n\n")
+        output_file.write(f"Text:\n{text}")
+
+    print("Extraction completed. Check content.txt for the result.")
