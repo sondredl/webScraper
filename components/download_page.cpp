4f597eb6315126a7df89331787367381e1d80634
Sondre Lind <sondredl@gmail.com>
2024-03-09 15:42:31 +0100

    begin implement cpp build system

---
+++ components/download_page.cpp
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+
+def downloadArticlePage(url):
+    path = "articles/"
+    output_file = path + str(increment_counter()) + ".html"
+
+    response = requests.get(url)
+    print(response)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        with open(output_file, "w", encoding="utf-8") as file:
+            file.write(str(soup))
+
+        print(f"Webpage content saved to {output_file}")
+    else:
+        print(f"Failed to retrieve webpage. Status code: {response.status_code}")
+
+
+def download_all_article_pages():
+    conn = sqlite3.connect("your_database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT href FROM WordAndUrl")
+    urls = cursor.fetchall()
+
+    cursor.execute("SELECT id FROM WordAndUrl")
+    ids = cursor.fetchall()
+
+    for url in urls:
+        downloadArticlePage(url[0])
+
+    conn.close()
+
+
+def increment_counter(counter=[0]):
+    counter[0] += 1
+    return counter[0]
+
+
+# download_all_article_pages()
---
+++ components/jsonParser.cpp
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import json
+
+
+def webPages():
+    json_file_path = "inputData/webPages.json"
+    with open(json_file_path, "r") as json_file:
+        web_pages = json.load(json_file)
+
+    webPage_array = []
+    for page in web_pages:
+        pageData = [page["name"], page["url"]]
+        webPage_array.append(pageData)
+    return webPage_array
+
+
+def htmlTags():
+    json_file_path = "inputData/htmlTags.json"
+    with open(json_file_path, "r") as json_file:
+        htmlTags = json.load(json_file)
+
+    tag_array = []
+
+    for tag in htmlTags:
+        tag_array.append(tag)
+    return tag_array
+
+
+def searchWords():
+    json_file_path = "inputData/searchWords.json"
+    with open(json_file_path, "r") as json_file:
+        word_list = json.load(json_file)
+
+    word_array = []
+
+    for word in word_list:
+        word_array.append(word)
+    return word_array
+
+
+def companyNames():
+    json_file_path = "inputData/companies.json"
+    with open(json_file_path, "r") as json_file:
+        word_list = json.load(json_file)
+
+    word_array = []
+
+    for word in word_list:
+        word_array.append(word)
+    return word_array
