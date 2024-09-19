b7eb9e531c353cec683725f6b920b56c31e5c592
Sondre Lindkjolen <sondredl@gmail.com>
2024-09-19 21:44:57 +0200

    initial implementation

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import time
 from src.dataExtractor      import dataExtractor
 from src.dbCleaner          import databaseCleaner
+from src.indexGetter          import indexExtractor
 
 def main():
     while True:
 
         data_Extractor = dataExtractor()
         data_Cleaner = databaseCleaner()
+        index_extractre = indexExtractor()
 
-        data_Extractor.download_web_pages() # downloads news pages
-        data_Extractor.updateDatabase("temp.db")     # get articles from news pages and store url to database searc_word
-        data_Extractor.getWordAndUrl("temp.db")      # filter articles in word_and_url to get the ones with search word hits
-        data_Extractor.getCompanyAndUrl("temp.db")   # filter articles in word_and_url to get the ones with search word hits
+        data_Extractor.download_web_pages()         # downloads news pages
+        data_Extractor.updateDatabase("temp.db")    # get articles from news pages and store url to database searc_word
+        data_Extractor.getWordAndUrl("temp.db")     # filter articles in word_and_url to get the ones with search word hits
+        data_Extractor.getCompanyAndUrl("temp.db")  # filter articles in word_and_url to get the ones with search word hits
 
         data_Extractor.cleanDuplicates("temp.db")
         data_Extractor.download_all_article_pages("temp.db") # downloads articles from word_and_url to file
@@ -21,6 +23,8 @@ def main():
         data_Extractor.m_dbCleaner.delete_folder_contents("htmlFiles")
         # data_Extractor.m_dbCleaner.set_last_time_run()
         # data_Cleaner.delete_all_content_in_table("temp.db", "raw_articles")
+
+        data_Extractor.download_web_pages() # downloads news pages
 
         sleep_time = 7200 # seconds
         time.sleep(sleep_time)
