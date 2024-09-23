6ceba0797423050a563774bcdfa7846d29bff81f
Sondre Lindkjolen <sondredl@gmail.com>
2024-09-23 20:19:12 +0200

    successfully downloads e24 markedsoversikt

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
 import subprocess
 
-subprocess.run(["make"])
+# subprocess.run(["make"])
+
+
+def download_web_pages(name, url):
+    filename = name + ".html"
+    path = "htmlFiles/"
+    path += filename
+    print(f"\n downloading {url} to {path}")
+    subprocess.run(["curl", "-L", "-o", path, url])
+
+download_web_pages("e24aksjer", "https://e24.no/bors")
