643a26abb14d6a31fd3bf128b5e8a394ab594aaf
Sondre Lindkjolen <sondredl@gmail.com>
2024-09-23 22:36:44 +0200

    tidy#2 #

--- src/getStockValue.py
+++ src/getStockValue.py
@@ -25,34 +25,43 @@ class aksjer24:
             with open(file_path, "r", encoding="utf-8") as file:
                 soup = BeautifulSoup(file, "html.parser")
 
+    def get_winners(filename):
+        folder_path = "htmlFiles/"
+        if filename.endswith(".html"):
+            file_path = os.path.join(folder_path, filename)
+
+            with open(file_path, "r", encoding="utf-8") as file:
+                soup = BeautifulSoup(file, "html.parser")
+
+    def get_loosers(filename):
+        folder_path = "htmlFiles/"
+        if filename.endswith(".html"):
+            file_path = os.path.join(folder_path, filename)
+
+            with open(file_path, "r", encoding="utf-8") as file:
+                soup = BeautifulSoup(file, "html.parser")
 
 
-def get_content_element_from_file(file_name, element_type, element_class):
+def get_content_element_from_file(file_name,  element_class):
     with open(file_name, "r", encoding="utf-8") as file:
         html_content = file.read()
 
     soup = BeautifulSoup(html_content, "lxml")
+    section = soup.find(True, class_=element_class)
 
-    # Find the section element with the specific class
-    section = soup.find(element_type, class_=element_class)
-
-    # If the section exists, proceed to find nested elements
     if section:
-        # Search for the nested 'a' element with the specific class within the section
         nested_element_class = "styles_item__kyxZ3"
-        nested_element_type = "a"
-        nested_elements = section.find_all(nested_element_type, class_=nested_element_class)
+        nested_elements = section.find_all(True, class_=nested_element_class)
 
-        # Print the nested element if found
         if nested_elements:
             for i, element in enumerate(nested_elements, 1):
-                element_content = element.get_text(strip=True)  # Get text content of each element
+                element_content = element.get_text(strip=True) 
                 print(f"Element {i}: {element_content}")
 
         else:
-            print(f"No nested element <{nested_element_type}> with class '{nested_element_class}' found.")
+            print(f"No nested element with class '{nested_element_class}' found.")
     else:
-        print(f"No element <{element_type}> with class '{element_class}' found.")
+        print(f"No element with class '{element_class}' found.")
 
 
 dataContainerClass1 = "styles_item__kyxZ3"
@@ -66,4 +75,4 @@ fileName = "htmlFiles/test.html"
 elementType = "section"
 elementClass = "overview_section__K48df overview_main__sZ_nI"
 
-get_content_element_from_file(fileName, elementType, elementClass)
+get_content_element_from_file(fileName, elementClass)
