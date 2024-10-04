a0d3ee10bc453127b23385bf728e0334accb9ec2
Sondre Lindkjolen <sondredl@gmail.com>
2024-10-04 18:25:08 +0200

    make function to print all unique company names from stock_index

--- visualizer.py
+++ visualizer.py
@@ -7,10 +7,7 @@ import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
 
-# Sample word list
-word_list = ['python', 'data', 'analysis', 'science', 'machine']
 
-# Step 1: Connect to SQLite database and fetch articles
 def get_articles_from_db(db_path):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -24,7 +21,6 @@ def get_articles_from_db(db_path):
     # Flatten the list of tuples into a list of strings
     return [article[0] for article in articles]
 
-# Step 2: Count occurrences of each word in the articles
 def count_word_occurrences(word_list, articles):
     word_count = Counter()
     
@@ -51,29 +47,22 @@ def plot_word_occurrences(word_count):
 
 
 def print_unique_company_names(db_name, table_name, column_name):
-    # Connect to the SQLite database
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
 
-    # Query to get unique company names
     cursor.execute(f"""SELECT DISTINCT {column_name} 
-                   FROM {table_name}""")  # Replace 'your_table_name' with the actual table name
+                   FROM {table_name}""")  
 
-    # Fetch all unique values
     unique_companies = cursor.fetchall()
 
-    # Print each unique company name
     for company in unique_companies:
         print(company[0])
 
-    # Close the connection
     conn.close()
 
-# Usage
 
-# Main function
 def main():
-    db_path = 'temp.db'  # Change this to your actual database path
+    db_path = 'temp.db'  
     articles = get_articles_from_db(db_path)
     
     with open("inputData/companies.json") as json_file:
