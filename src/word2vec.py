8084a539b2f1cab86b5e1eb3aee54efa46e042ae
Sondre Lindkjolen <sondredl@gmail.com>
2024-10-04 18:11:15 +0200

    removes comments

--- src/word2vec.py
+++ src/word2vec.py
@@ -46,18 +46,13 @@ class vectorizeText:
         row = cursor.fetchone()
         if row:
             title, subtitle, text = row
-            # sentences = [line.split() for line in text]
             sentences = [sentence.strip().split() for sentence in text.split('.') if sentence.strip()]
             all_words = [word for sentence in sentences for word in sentence]
 
 
-            # w2v = Word2Vec(sentences, vector_size=100, window=5, workers=4, min_count=5)
             w2v = Word2Vec(vector_size=100, window=5, workers=4, min_count=1)
             w2v.build_vocab([all_words])
-            # w2v.build_vocab(sentences)
             w2v.train(sentences, total_examples=w2v.corpus_count, epochs=10)
-            # for sentence in sentences:
-            #     print(f"\n\nsentence {sentence}")
             for word in w2v.wv.index_to_key:
                 print(f"word: {word}\nVector: \n{w2v.wv[word]}\n")
 
@@ -75,19 +70,6 @@ class vectorizeText:
         conn.close()
         for row in rows:
             self._extract_sentences_from_article(database_name, table_name, row, row[0])
-            # title, subtitle, text = row
-            # sentences = [line.split() for line in text]
-            # sentences = [sentence.strip().split() for sentence in text.split('.') if sentence.strip()]
-            # all_words = [word for sentence in sentences for word in sentence]
-
-
-            # # w2v = Word2Vec(sentences, vector_size=100, window=5, workers=4, min_count=5)
-            # w2v = Word2Vec(vector_size=100, window=5, workers=4, min_count=1)
-            # w2v.build_vocab([all_words])
-            # # w2v.build_vocab(sentences)
-            # w2v.train(sentences, total_examples=w2v.corpus_count, epochs=10)
-            # for sentence in sentences:
-            #     print(f"\n\nsentence {sentence}")
 
     def _extract_sentences_from_article(self, database_name, table_name, row_content, index):
         connection = sqlite3.connect(database_name)
@@ -124,20 +106,6 @@ class vectorizeText:
         rows = cursor.fetchall()
         conn.close()
         for row in rows:
-            ###### get vector ##########
-            # title, subtitle, text = row
-            # sentences = [sentence.strip().split() for sentence in text.split('.') if sentence.strip()]
-            # all_words = [word for sentence in sentences for word in sentence]
-            # w2v = Word2Vec(vector_size=100, window=5, workers=4, min_count=1)
-            # w2v.build_vocab([all_words])
-            # w2v.train(sentences, total_examples=w2v.corpus_count, epochs=10)
-            # for word in all_words:
-            #     word_vector = w2v.wv[word]
-            #     print(word)
-            #     print(word_vector)
-                # print(".")
-                # print(word)
-            ################################
             self._extract_word_from_sentence(database_name, table_name, row, row[0])
 
     def _extract_word_from_sentence(self, database_name, table_name, row_content, index):
@@ -159,7 +127,10 @@ class vectorizeText:
 
             words = re.findall(r'\b\w+\b', sentence)
 
-            w2v = Word2Vec(vector_size=100, window=5, workers=4, min_count=1)
+            w2v = Word2Vec(vector_size=100, 
+                           window=5, 
+                           workers=4, 
+                           min_count=1)
             w2v.build_vocab([words])
             w2v.train(sentence, total_examples=w2v.corpus_count, epochs=10)
 
